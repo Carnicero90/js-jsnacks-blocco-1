1f2a131464602cef32ad07f0da265a5a6494ad3d
Calavera <calavera@localhost.localdomain>
2021-04-13 14:28:08 +0200

    resa generica (e non più spuria) funzione sumObjectsValues (ex funzione sommaZucche), aggiunto qualche commento di documentazione

--- js/script.js
+++ js/script.js
@@ -1,25 +1,28 @@
 var tipiZucchine = ['bianca', 'gialla', 'rossa', 'striata', 'succulenta', 'essiccata'];
 
+// GENERATE ARRAY OF OBJECTS WITH RANDOM VALUES
 var zucchine = Array.from({length: 10}, i => i={
   'tipo': getRandomItem(tipiZucchine),
   'peso':  getRandomInt(1,15),
   'lunghezza': getRandomInt(1,30)
 });
 
-var sommaTotale = pesaZucche(zucchine);
+var sommaTotale = sumObjectsValues(zucchine, 'peso');
 
+// SPLIT zucchine ITEMS INTO corte AND lunghe (ACCORDING TO lunghezza)
 var corte = [];
 var lunghe= [];
-
 zucchine.filter((zucchina) => zucchina.lunghezza>15 ? lunghe.push(zucchina) : corte.push(zucchina));
 
-var sommaCorte = pesaZucche(corte);
-var sommaLunghe = pesaZucche(lunghe);
+var sommaCorte = sumObjectsValues(corte, 'peso');
+var sommaLunghe = sumObjectsValues(lunghe, 'peso');
+// SAFETY CHECK
 console.log(sommaCorte === sommaTotale - sommaLunghe);
 
 
-function pesaZucche(array) {
-  return array.reduce((somma, zucchina) => somma + zucchina.peso, 0)
+function sumObjectsValues(array, prop) {
+  // RETURN THE SUM OF array[i].prop FOR i IN array
+  return array.reduce((sum, item) => sum + item[prop], 0)
 }
 
 function getRandomItem(array) {
